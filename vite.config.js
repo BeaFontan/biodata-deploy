09fb03cfac9d9957f52ee858214eeb94455f6792
Beatriz Fontán <beatriz.fontan@squareet.com>
2025-06-15 20:11:20 +0200

    deplk

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    base: process.env.ASSET_URL ? new URL(process.env.ASSET_URL).pathname : '/build/',
+    base: '/build/',
+    build: {
+        assetsDir: '', // <- esta línea pone los archivos directamente en /build
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/css/auth.css', 'resources/js/app.js'],
